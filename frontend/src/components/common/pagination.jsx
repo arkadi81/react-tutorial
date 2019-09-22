0d2fe4fc708dee97f6d61b4287027e9a9c64da39
arkadi <arkadi81@gmail.com>
2019-09-22 12:58:02 -0700

    added lodash, building pagination component

--- frontend/src/components/common/pagination.jsx
+++ frontend/src/components/common/pagination.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+/*Interface:
+inputs: itemsCount - number of total items
+        pageSize - the amount of items on each oage
+events: onPageChange(newPageNumber) - raised by this component whenever the page number changeson
+output:
+*/
+
+// lodash@4.17.10 installed to facilitate the creation of the pages array
+//algorithm - using page size and total items create an array of page numbers. map array to bootstrap buttons
+
 const Pagination = props => {
   const { itemsCount, pageSize, onPageChange, currentPage } = props;
   const pagesCount = Math.ceil(itemsCount / pageSize); // so we don't run into floating point issues
@@ -22,36 +32,28 @@ const Pagination = props => {
       <nav aria-label="Page navigation example">
         <ul className="pagination">
           <li className="page-item">
-            <a className="page-link" href="#">
-              First (TODO)
+            <a className="page-link" onClick={() => props.onPageChange(1)}>
+              Previous
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#">
-              Previous (TODO)
-            </a>
-          </li>
-          {pagesArr.map(pageNum => (
-            <li
-              className={
-                pageNum === currentPage ? "page-item active" : "page-item"
-              }
-              key={pageNum}
-            >
-              <a className="page-link" onClick={() => onPageChange(pageNum)}>
-                {pageNum}
-              </a>
-            </li>
-          ))}
-
-          <li className="page-item">
-            <a className="page-link" href="#">
-              Next (TODO)
+            <a className="page-link" onClick={() => props.onPageChange(1)}>
+              1
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#">
-              Last (TODO)
+            <a className="page-link" onClick={() => props.onPageChange(1)}>
+              2
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" onClick={() => props.onPageChange(1)}>
+              3
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" onClick={() => props.onPageChange(1)}>
+              Next
             </a>
           </li>
         </ul>
--- frontend/src/components/movies.jsx
+++ frontend/src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    pageSize: 4
   };
 
   handleDelete = _id => {
@@ -17,6 +19,15 @@ class Movies extends Component {
   handleLike = movie => {
     console.log("like clicked: ", movie);
     const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  handlePageChange = newPageNumber => {
+    //raised when a pagination element is clicked and page is changed
+    console.log(newPageNumber);
   };
 
   render() {
@@ -62,6 +73,11 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
